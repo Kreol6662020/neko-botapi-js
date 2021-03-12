f81d97dfa4e42eb16dab60fea43af49322e9ade1
Креол <70958705+Kreol6662020@users.noreply.github.com>
2021-03-12 16:36:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const api = new NekoBot();
 // Не забудьте про npm i nekobot-api
 console.log("script by Kreol");
 (async () => {
-    const image = await api.generate("magik", { image: "image url" });
+    const image = await api.generate("food", { image: "image url" });
     console.log(image);
 })();
